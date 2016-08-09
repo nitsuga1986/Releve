ab767c3249bfb95591b88e613cddedfaa3fdb482
nitsuga1986 <agustinmancenido@gmail.com>
2016-08-08 21:26:28 -0300

    Twit Face

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 module Releve
   class Application < Rails::Application
 	# Enable the asset pipeline
+	config.assets.initialize_on_precompile = false
 	config.assets.enabled = true
 	config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
 	config.assets.precompile += %w(
