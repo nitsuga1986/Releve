15c20b4eb46b15bb96642f56d294ad2b314638a0
nitsuga1986 <agustinmancenido@gmail.com>
2016-08-09 11:01:29 -0300

    Logout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,8 +249,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  # config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
-  config.omniauth :twitter, '3oVHVbxKJVa02nzt6aMpqj0Rx', 'GlXr5XWIDP3EQBpL5KahjyTL3q8HAV01VDFlC4vQXDBcRPBXO9'
+  config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
