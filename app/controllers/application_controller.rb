ddfd5ab7eb188bd72a586e42d1a099e1a5a2f464
unknown <am844n@ARSIS-AMANCENHP.americas.att.com>
2016-08-18 11:29:42 -0300

    showconfirmmodal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :authenticate_user!
+  before_action :check_confirmed
   protect_from_forgery with: :exception
-  
-  if user_signed_in? && !current_user.confirmed?
-	@show_confirm_form = true
+
+  private
+ 
+  def check_confirmed
+	  if user_signed_in? && !current_user.confirmed?
+		@show_confirm_form = true
+	  end
   end
   
 end
