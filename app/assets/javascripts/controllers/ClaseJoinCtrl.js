cf9030f2bfda347c109154bc82ac1e38912f7960
nitsuga1986 <agustinmancenido@gmail.com>
2016-08-22 14:46:44 -0300

    detallesvarios

--- app/assets/javascripts/controllers/ClaseJoinCtrl.js
+++ app/assets/javascripts/controllers/ClaseJoinCtrl.js
@@ -40,73 +40,40 @@ angular.module("TurnosApp").controller("ClaseJoinCtrl", ['$scope', '$routeParams
 		tmpl_cache: false,
 		onAfterEventsLoad: function(events) {
 			if(!events) {return;}
-			var list = $('#eventlist');
-			list.html('');
-			var count=0;
-			$.each(events, function(key_event, event) {
-				// Old_class
-				today = new Date();
-				fecha_clase = new Date(event.fecha);
-				if( fecha_clase > today) {	events[key_event].old_class = false;
-				} else {						events[key_event].old_class = true;}
-				// Each user
-				$.each(event.users, function(key_user, user) {
-					//Joined
-					if(user.id == $scope.user_id) {	events[key_event].joined = true;
-					} else {						events[key_event].joined = false;}
-				});
-				if (events[key_event].old_class == false){
-					count+=1;
-					if (count<10){
-						if (event.joined!=true){
-						$(document.createElement('a')).addClass("eventlist btn btn-default").attr('type', 'button')
-						.attr('data-event-id', event.id)
-						.html('<i title="Click para anotarse" class="fa fa-circle pull-left text-'+event.class+'" aria-hidden="true"></i> '+event.title).appendTo(list);}
-						else {
-						$(document.createElement('a')).addClass("eventlist btn btn-default").attr('type', 'button')
-						.attr('data-event-id', event.id)
-						.html('<i title="Anotado!" class="fa fa-check-square pull-left text-'+event.class+'" aria-hidden="true"></i>'+event.title).appendTo(list);}
-						
-					}
-				} 
-			});
-			if (count==0){$(document.createElement('p')).html('No hay clases disponibles en este momento').appendTo(list);}
-			$scope.clases = events;
+			eventsLoader(events);
 		},
 		onAfterViewLoad: function(view) {
-			$('h3.title').html("Turnos <span class='muted'>· "+this.getTitle()+"</span>");
-			$('.btn-group button').removeClass('active');
+			// Class active: btn-group-view 
+			$('.btn-group-view button').removeClass('active');
 			$('button[data-calendar-view="' + view + '"]').addClass('active');
-			// Prev-Next
+			// Prev-Next buttons
 			$('.btn-group button[data-calendar-nav]').each(function() {
 				var $this = $(this);
 				$this.unbind().click(function() {
 					calendar.navigate($this.data('calendar-nav'));
 				});
 			});
-			// Calendar view
+			// Calendar view buttons
 			$('.btn-group button[data-calendar-view]').each(function() {
 				var $this = $(this);
 				$this.unbind().click(function() {
 					calendar.view($this.data('calendar-view'));
 				});
 			});
-			// Event click. $scope.clases[index] => $scope.clase
-			$(document.body).on('click', 'a[data-event-id]', function(){
-				console.log('angularjsevent');
+			// Clase selected => to Modal
+			$(document.body).on('click', 'a[data-event-id].setClase', function(){
 				id = $(this).attr('data-event-id');
-				$.each($scope.clases, function(index) {
-					if($scope.clases[index].id == id) {
-						$scope.clase = $scope.clases[index];
-						$scope.$apply() 
-						return false;
-					}    
-				});
+				setClaseModal(id);
 				$('#events-modal').modal('toggle')
 			});
 		},
 		onAfterSlideLoad: function(view) {
-			console.log("onAfterSlideLoad called");
+			$('ul.list-unstyled > li[data-event-id].setClase').on('click', function(event){
+				id = $(this).attr('data-event-id');
+				setClaseModal(id);
+				$('#events-modal').modal('toggle');
+				return false;
+			});
 		},
 		classes: {
 			months: {
@@ -116,6 +83,67 @@ angular.module("TurnosApp").controller("ClaseJoinCtrl", ['$scope', '$routeParams
 		week_numbers_iso_8601: true,
 	};
 
+	
+	// setClaseModal
+	function setClaseModal(id) {
+		$.each($scope.clases, function(index) {
+			if($scope.clases[index].id == id) {
+				$scope.clase = $scope.clases[index];
+				$scope.$apply();
+				return false;
+			}    
+		});
+	}
+	function dateFormat(date) {
+		date = date.split('-')
+		date = date[2]+'/'+date[1]
+		return date
+
+	}
+	// eventsLoader
+	function eventsLoader(events) {
+		var list = $('#eventlist').html('');
+		var mylist = $('#myeventlist').html('');
+		// All events
+		var old_clase_count=0;
+		var my_clase_count=0;
+		$.each(events, function(key_event, event) {
+			// Old_clase?
+			today = new Date();
+			fecha_clase = new Date(event.fecha);
+			if( fecha_clase > today) {	events[key_event].old_clase = false;
+			} else {					events[key_event].old_clase = true; events[key_event].class = 'default';}
+			// Each user in event
+			$.each(event.users, function(key_user, user) {
+				//Joined
+				if(user.id == $scope.user_id) {	events[key_event].joined = true;
+				} else {						events[key_event].joined = false;}
+			});
+			if (events[key_event].old_clase == false){
+				old_clase_count+=1;
+				if (old_clase_count<10){
+					if (event.joined!=true){
+					$(document.createElement('a')).addClass("eventlist btn btn-default setClase").attr('type', 'button')
+					.attr('data-event-id', event.id)
+					.html('<i title="Click para anotarse" class="fa fa-circle text-'+event.class+'" aria-hidden="true"></i> '+dateFormat(event.fecha)+' '+event.horario+'hs: '+event.actividad).appendTo(list);}
+					else {
+					$(document.createElement('a')).addClass("eventlist btn btn-default setClase").attr('type', 'button')
+					.attr('data-event-id', event.id)
+					.html('<i title="Anotado!" class="fa fa-check-square text-'+event.class+'" aria-hidden="true"></i> '+dateFormat(event.fecha)+' '+event.horario+'hs: '+event.actividad).appendTo(list);}
+					
+				}
+			} 
+			if (events[key_event].joined == true){
+				my_clase_count+=1;
+				$(document.createElement('a')).addClass("eventmylist btn btn-default setClase").attr('type', 'button')
+				.attr('data-event-id', event.id)
+				.html('<i title="Anotado!" class="fa fa-check-square text-'+event.class+'" aria-hidden="true"></i> '+dateFormat(event.fecha)+' '+event.horario+'hs: '+event.actividad).appendTo(mylist);
+			} 
+		});
+		if (old_clase_count==0){$(document.createElement('p')).html('No hay clases disponibles en este momento').appendTo(list);}
+		if (my_clase_count==0){$(document.createElement('p')).html('No tienes clases programadas =(').appendTo(mylist);}
+		$scope.clases = events;
+	}
 	var calendar = $('#calendar').calendar(options);
 
 }]);
