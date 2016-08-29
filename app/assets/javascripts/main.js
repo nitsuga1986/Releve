482583fd069850d85770b8f385d82c8b1430f014
nitsuga1986 <am844n@ARSIS-AMANCENHP.americas.att.com>
2016-08-29 16:23:41 -0300

    carrousel

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -46,7 +46,7 @@ jQuery(function($) {
 		});
 		$("#gallery-carrousel").owlCarousel({
 			autoPlay: 3000, //Set AutoPlay to 3 seconds
-			items : 4,
+			items : 3,
 			itemsDesktop : [1199,3],
 			itemsDesktopSmall : [979,3]
 		});
