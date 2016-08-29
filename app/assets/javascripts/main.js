404632f9c490d16af5ccf2620280a21bdffd6884
nitsuga1986 <am844n@ARSIS-AMANCENHP.americas.att.com>
2016-08-29 15:52:28 -0300

    carrousel

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -30,7 +30,7 @@ jQuery(function($) {
 			else if ($(this).hasClass('cut-bottom'))
 				$(this).css('border-left-width', $(this).parent().width() + "px");
 		});
-		$('#carrousel-container').height($(window).height()*0.8);
+		$('#carrousel-container').height($(window).height()*0.5);
 
 		// Sliders Init
 		$('.owl-schedule').owlCarousel({
