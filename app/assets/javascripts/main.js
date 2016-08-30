8776d31837f382e98f0370fea3d42d5106cf3f6b
nitsuga1986 <am844n@ARSIS-AMANCENHP.americas.att.com>
2016-08-30 11:07:37 -0300

    quotes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -8,13 +8,13 @@ jQuery(function($) {
 	// Window Load
 	$(window).load(function() {
 		// Preloader
-	// 	$('.intro-tables, .parallax, header').css('opacity', '0');
-	// 	$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-	// 		$('.preloader').hide();
-	// 		$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-	// 			$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-	// 		});
-	// 	});
+		$('.intro-tables, .parallax, header').css('opacity', '0');
+		$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+			$('.preloader').hide();
+			$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+			});
+		});
 
 		// Header Init
 		if ($(window).height() > $(window).width()) {
