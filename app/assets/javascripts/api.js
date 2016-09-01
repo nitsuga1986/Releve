19ca972d9b4c32daf8e0477ffdd37bf5f4170267
nitsuga1986 <agustinmancenido@gmail.com>
2016-09-01 18:04:41 -0300

    javascriptfix

---
+++ app/assets/javascripts/api.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-ui/datepicker
+//= require jquery-ui/autocomplete
+//= require angular/angular.min
+//= require angular/angular-resource.min
+//= require angular/angular-route.min
+//= require angular/ng-table.min
+//= require config
+//= require filters/html_trusted
+//= require services/resources
+//= require controllers/ClaseIndexCtrl
+//= require controllers/ClaseEditCtrl
+//= require controllers/ClaseShowCtrl
+//= require controllers/ClaseJoinCtrl
+//= require controllers/AlumnoIndexCtrl
+//= require controllers/AlumnoEditCtrl
+//= require controllers/AlumnoShowCtrl
+//= require bootstrap-calendar/underscore-min
+//= require bootstrap-calendar/calendar
+//= require routes
+
+
