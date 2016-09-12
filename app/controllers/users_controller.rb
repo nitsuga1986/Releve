633a7753d1a9af8c1b7c5ba340a915b508c16969
nitsuga1986 <agustinmancenido@gmail.com>
2016-09-12 15:24:03 -0300

    indexesfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
     if request.patch? && params[:user] # Revisa si el request es de tipo patch, es decir, llenaron el formulario y lo ingresaron
       @user = User.find(params[:id])
       if params[:user][:email].present? && params[:user][:telefono].present?
+		UserMailer.welcome_email(@user).deliver if @user.confirmed == false
 		@user.update_attribute(:confirmed, true)
 		@user.update(user_params)
         bypass_sign_in(@user)
-        UserMailer.welcome_email(@user).deliver
         redirect_to '/dashboard/agenda/', notice: 'Hemos guardado tu email correctamente.'
       else
 		@landingpage=true
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
  
   private
     def user_params
-      accessible = [:email, :dni, :nombre, :apellido, :profesion, :fechanac, :fechaini, :telefono, :domicilio, :localidad, :nombre_contacto, :apellido_contacto, :telefono_contacto, :sexo] # extend with your own params
+      accessible = [:email, :telefono]
       accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
       params.require(:user).permit(accessible)
     end
