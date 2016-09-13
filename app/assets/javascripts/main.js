b88db435fee25f3776061fbc7ff138dd6a65c357
nitsuga1986 <am844n@ARSIS-AMANCENHP.americas.att.com>
2016-09-13 14:53:21 -0300

    landingeffect

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -96,14 +96,7 @@ jQuery(function($) {
 			scrollSpeed: 400,
 			filter: ':not(.btn)'
 		});
-		// Preloader
-		$('.intro-tables, .parallax, header').css('opacity', '0');
-		$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-			$('.preloader').hide();
-			$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-				$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-			});
-		});
+
 		// Typing Intro Init
 		$(".typed").typewriter({
 			speed: 60
