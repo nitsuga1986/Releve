060738d396b2d803071355f02a0549e7375d5e17
nitsuga1986 <agustinmancenido@gmail.com>
2016-09-14 00:44:50 -0300

    landingtransition

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,5 +1,4 @@
 //= require owl/owl.carousel.min
 //= require wow.min
-//= require typewriter
 //= require jquery.onepagenav
 //= require main
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -100,20 +100,20 @@ jQuery(function($) {
 			scrollSpeed: 400,
 			filter: ':not(.btn)'
 		});
+		
+
 		// Preloader
 		$('.intro-tables, .parallax, header').css('opacity', '0');
 		$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
 			$('.preloader').hide();
 			$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-				$('#sp-container').addClass('sp-container');
 				$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+				$('#intro > .container').addClass('run-animation').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
 			});
-		});
-		// Typing Intro Init
-		$(".typed").typewriter({
-			speed: 60
 		});
 	});
 
 
 });
+
+
