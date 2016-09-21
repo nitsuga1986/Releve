23210807e1a948b4bf6e4b3dc6685c1058b3527e
nitsuga1986 <agustinmancenido@gmail.com>
2016-09-21 00:08:47 -0300

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,6 @@ Rails.application.configure do
 	config.action_mailer.raise_delivery_errors = false
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.default_url_options = { :host => 'http://www.relevepilates.com.ar/' }
-	config.action_mailer.asset_host = 'http://www.relevepilates.com.ar/'
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
 	  address:              'smtp.gmail.com',
