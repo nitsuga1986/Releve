7b251ffaaaedb5844c4d4c9bf65217db282e673c
nitsuga1986 <agustinmancenido@gmail.com>
2016-09-28 11:49:31 -0300

    markup

---
+++ app/assets/javascripts/markupOrganization.js
@@ -0,0 +1,20 @@
+//schema.org markup
+{
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "url": "http://www.relevepilates.com.ar/",
+  "logo": "http://www.relevepilates.com.ar/logo.png",
+  "contactPoint": [{
+    "@type": "ContactPoint",
+    "telephone": "+54 (011) 4382 - 6446",
+    "contactType": "customer service",
+    "areaServed": "AR",
+    "availableLanguage": ["Spanish","English"]
+  }],
+  "sameAs": [
+    "https://www.facebook.com/relevepilates/",
+    "https://www.instagram.com/relevepilates/",
+    "https://plus.google.com/+RelevePilatesBuenosAires/",
+    "https://www.youtube.com/channel/UCANspzZurd6hdJqrJPZBU8w"
+  ]
+}
