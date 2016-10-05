dcfe9528333f61b7842703ec041096328c660bae
nitsuga1986 <agustinmancenido@gmail.com>
2016-10-04 21:33:33 -0300

    instructorapp

--- app/assets/javascripts/controllers/ClaseInstructorCtrl.js
+++ app/assets/javascripts/controllers/ClaseInstructorCtrl.js
@@ -1,64 +1,46 @@
 angular.module("TurnosApp").controller("ClaseInstructorCtrl",['$scope', '$rootScope', '$q', '$http', '$routeParams', '$location', 'ResourceClase', 'ResourceActividad', 'ResourceAlumno', '$filter', 'NgTableParams', function($scope, $rootScope, $q, $http, $routeParams, $location, ResourceClase, ResourceActividad, ResourceAlumno, $filter, NgTableParams) {
 	// SetDay
-	SetDay = function(minusMonth) {
+	SetDay = function(plusDays) {
 		today = new Date();
-		dd = today.getDate();if(dd<10){dd='0'+dd } 
-		mm = today.getMonth()+1-minusMonth;if(mm<10){mm='0'+mm }  //January is 0!
+		dd = today.getDate()+plusDays;if(dd<10){dd='0'+dd } 
+		mm = today.getMonth()+1;if(mm<10){mm='0'+mm }  //January is 0!
 		yyyy = today.getFullYear();
 		return yyyy+'-'+mm+'-'+dd
 	};
 	function dateFormat(date) {date = date.split('-'); date = date[2]+'/'+date[1]; return date;}
-	// From
-	$scope.GoToEdit = function(id) {$location.path("/clase/"+id+"/edit/");};
-	$scope.columns_instructor = columns_instructor;
-	$scope.clase = new ResourceClase();
-	$scope.InstructorIndex = ResourceAlumno.instructores();
-	$scope.clase.fecha_end = SetDay(0);
-	$scope.clase.fecha_start = SetDay(1);
-	$scope.InstructorIndex.$promise.then(function(data) {
-		$scope.clase.instructor_id = $scope.InstructorIndex[InstructorIndexDefault].id;
-	});
-	$scope.FormButton = '<i class="fa fa-search"></i> Buscar';
 
 	// SUBMIT
 	$scope.submitted = false;
 	$scope.submit = function() {
 		$rootScope.got_to_url_success = "/clase/instructor";
 		$scope.FormErrors = [];
-		if ($scope.ClaseForm.$valid) {
-			console.log("valid submit");
-
-			$scope.tableParams = new NgTableParams({
-				page: claseInstructorPage,         	// initial first page
-				count: claseInstructorCount,         	// initial count per page
-				filter: claseInstructorFilter, 		// initial filter
-				sorting: claseInstructorSorting, 		// initial sorting
-			}, {
-				total: 0,           				// length of data
-				counts: clasePageSizes,				// page size buttons
-				getData: function(params) {
-					// ajax request to api
-					startLoading();
-					return ResourceClase.instructor($scope.clase).$promise.then(function(data) {
-						angular.forEach(data, function(value, key) {
-							data[key]["cant_users"] = value.users.length+" / "+value.max_users;
-							data[key]["fecha_fixed"] = value.dia+' ' +dateFormat(value.fecha);
-						});
-						$scope.clases = data;
-						// Filter & Sort
-						filteredData = params.filter() ? $filter('filter')(data, params.filter()): data;	
-						orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : filteredData;
-						// Show
-						params.total(orderedData.inlineCount);
-						stopLoading();
-						return orderedData;
+		$scope.tableParams = new NgTableParams({
+			page: claseInstructorPage,         	// initial first page
+			count: claseInstructorCount,         	// initial count per page
+			filter: claseInstructorFilter, 		// initial filter
+			sorting: claseInstructorSorting, 		// initial sorting
+		}, {
+			total: 0,           				// length of data
+			counts: clasePageSizes,				// page size buttons
+			getData: function(params) {
+				// ajax request to api
+				startLoading();
+				return ResourceClase.instructor($scope.clase).$promise.then(function(data) {
+					angular.forEach(data, function(value, key) {
+						data[key]["cant_users"] = value.users.length+" / "+value.max_users;
+						data[key]["fecha_fixed"] = value.dia+' ' +dateFormat(value.fecha);
 					});
-				}
-			});
-		} else {
-			$scope.ClaseForm.submitted = true;
-			window.scrollTo(0, 0);
-		}
+					$scope.clases = data;
+					// Filter & Sort
+					filteredData = params.filter() ? $filter('filter')(data, params.filter()): data;	
+					orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : filteredData;
+					// Show
+					params.total(orderedData.inlineCount);
+					stopLoading();
+					return orderedData;
+				});
+			}
+		});
 	};
 
 	$scope.searchToday = function() {
@@ -99,4 +81,19 @@ angular.module("TurnosApp").controller("ClaseInstructorCtrl",['$scope', '$rootSc
 			});
 		})
 	};
+	// Defaults
+	$scope.GoToEdit = function(id) {$location.path("/clase/"+id+"/edit/");};
+	$scope.columns_instructor = columns_instructor;
+	$scope.clase = new ResourceClase();
+	$scope.InstructorIndex = ResourceAlumno.instructores();
+	$scope.FormButton = '<i class="fa fa-search"></i> Buscar';
+	$scope.InstructorIndex.$promise.then(function(data) {
+		if (typeof $scope.is_instructor !== 'undefined' && $scope.is_instructor){	
+				$scope.clase.instructor_id = $scope.instructor_id;}
+		else{	$scope.clase.instructor_id = $scope.InstructorIndex[InstructorIndexDefault].id;}
+		$scope.clase.fecha_end = SetDay(+5);
+		$scope.clase.fecha_start = SetDay(-2);
+		$scope.submit();
+	});
+	
 stopLoading();}]);
