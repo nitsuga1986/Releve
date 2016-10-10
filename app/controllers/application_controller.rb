f85e62393e1cad3bce467568f34b36aecbf99823
nitsuga1986 <agustinmancenido@gmail.com>
2016-10-10 17:54:50 -0300

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :correct_domain!
   before_action :check_confirmed
   protect_from_forgery with: :exception
 
@@ -11,11 +10,6 @@ class ApplicationController < ActionController::Base
 	  if user_signed_in? && !current_user.confirmed?
 		@show_confirm_form = true
 	  end
-  end
-  def correct_domain!
-    unless request.host == 'www.relevepilates.com.ar'
-      redirect_to root_url, :status => 301  # or explicitly 'http://www.mysite.com/'
-    end
   end
   
 end
