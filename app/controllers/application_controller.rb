5c93bf3e15f373ad6fbed26af1bdb8d2769e5263
nitsuga1986 <agustinmancenido@gmail.com>
2016-10-10 18:00:01 -0300

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :ensure_domain
   before_action :check_confirmed
   protect_from_forgery with: :exception
 
@@ -12,4 +13,10 @@ class ApplicationController < ActionController::Base
 	  end
   end
   
+  APP_DOMAIN = 'www.relevepilates.com.ar'
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      redirect_to "http://#{APP_DOMAIN}", :status => 301	# HTTP 301 is a "permanent" redirect
+    end
+  end
 end
