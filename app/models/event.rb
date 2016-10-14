9a2712964b46fa7f68de473f3d5834b0fa91b57d
nitsuga1986 <agustinmancenido@gmail.com>
2016-10-14 16:32:25 -0300

    agendarmultiple

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   validate :count_within_limit, :on => :create
   
   def count_within_limit
-    if Event.all.count >= 100
+    if Event.all.count >= 200
 		Event.first.destroy
     end
   end
