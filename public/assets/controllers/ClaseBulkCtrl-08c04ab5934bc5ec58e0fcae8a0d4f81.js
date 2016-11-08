3225528d6dcf9330619f35b90de2b8a47274f615
nitsuga1986 <agustinmancenido@gmail.com>
2016-11-08 14:00:58 -0300

    AppFixed

---
+++ public/assets/controllers/ClaseBulkCtrl-08c04ab5934bc5ec58e0fcae8a0d4f81.js
@@ -0,0 +1,67 @@
+angular.module("TurnosApp").controller("ClaseBulkCtrl",['$scope', '$rootScope', '$q', '$http', '$routeParams', '$location', 'ResourceClase', 'ResourceActividad', 'ResourceAlumno', function($scope, $rootScope, $q, $http, $routeParams, $location, ResourceClase, ResourceActividad, ResourceAlumno) {
+	$scope.FormErrors = [];
+	$scope.horariosArray = horariosArray;
+	$scope.submiterror = false;
+	$scope.GoToNewActividad = function() {$location.path("/actividad/new");};
+	$scope.ActividadIndex = [];
+	$scope.ActividadIndex = ResourceActividad.index();
+	$scope.InstructorIndex = ResourceAlumno.instructores();
+	// SetDay
+	SetDay = function(plusDays) {
+		var currentDate = new Date(new Date().getTime() + plusDays * 24 * 60 * 60 * 1000);
+		dd = currentDate.getDate();if(dd<10){dd='0'+dd } 
+		mm = currentDate.getMonth()+1;if(mm<10){mm='0'+mm }  //January is 0!
+		yyyy = currentDate.getFullYear();
+		return yyyy+'-'+mm+'-'+dd
+	};
+	// Bulk
+	$scope.clase = new ResourceClase();
+	$scope.clase.fecha_start = SetDay(0);
+	$scope.clase.fecha_end = SetDay(30);
+	$scope.clase.max_users = 4;
+	$scope.clase.duracion = 1; 
+	$scope.ActividadIndex.$promise.then(function(data) {
+		$scope.clase.actividad_id = $scope.ActividadIndex[ActividadIndexDefault].id;
+	});
+	$scope.InstructorIndex.$promise.then(function(data) {
+		$scope.clase.instructor_id = $scope.InstructorIndex[InstructorIndexDefault].id;
+	});
+	// SUBMIT
+	$scope.submitted = false;
+	$scope.submit = function() {
+		$rootScope.got_to_url_success = "/clase/dashboard";
+		$scope.FormErrors = [];
+		if ($scope.ClaseForm.$valid) {
+			console.log("valid submit");
+			ResourceClase.bulk($scope.clase, $scope.callbackSuccess, $scope.callbackFailure);
+
+		} else {
+			$scope.ClaseForm.submitted = true;
+			window.scrollTo(0, 0);
+		}
+	};
+	// filterDayChange
+	$scope.clase.filterDay = new Array(59).fill(false);
+	$scope.filterDayChange = function(day,event) {
+		event.preventDefault();
+		$scope.clase.filterDay[day] = !$scope.clase.filterDay[day];
+	};
+	// Datepickers
+	 var datelist = []; // initialize empty array
+	 $(function() {
+		$( "#fecha_start" ).datepicker({
+			defaultDate: "+0d",
+			onSelect: function(dateText) {
+				$scope.clase.fecha_start=dateText;
+		   }
+		});
+	});
+	 $(function() {
+		$( "#fecha_end" ).datepicker({
+			defaultDate: "+1m",
+			onSelect: function(dateText) {
+				$scope.clase.fecha_end=dateText;
+		   }
+		});
+	});
+stopLoading();}]);
---
+++ public/assets/controllers/UsrMisClasesCtrl-8e80a5331f2d6f4f13fe894b9ddecfcc.js
@@ -0,0 +1,54 @@
+angular.module("TurnosApp").controller("UsrMisClasesCtrl",['$scope', '$rootScope', '$location', 'ResourceClase', 'ResourceAlumno', '$filter','NgTableParams', '$timeout', '$cacheFactory', function($scope, $rootScope, $location, ResourceClase, ResourceAlumno, $filter, NgTableParams, $timeout, $cacheFactory) {
+	ResourceAlumno.current().$promise.then(function(data) {
+		$scope.alumno = data;
+		// ngTable
+		function dateFormat(date) {date = date.split('-'); date = date[2]+' de '+monthNames[parseInt(date[1])-1]; return date;}
+		td = new Date();
+		var Api = ResourceClase;
+		$scope.columns_claseMisClases = columns_claseMisClases;
+		$scope.cant_visible_cols = $.grep(columns_claseMisClases, function(e){ return e.visible == true; }).length+1;
+		$scope.tableParams = new NgTableParams({
+			page: claseMisClasesDefaultPage,         	// initial first page
+			count: claseMisClasesDefaultCount,         	// initial count per page
+			filter: claseMisClasesDefaultFilter, 		// initial filter
+			sorting: claseMisClasesDefaultSorting,		// initial sorting
+		}, {
+			total: 0,          			 			// length of data
+			counts: claseMisClasesPageSizes,				// page size buttons
+			getData: function(params) {
+				// ajax request to api
+				startLoading();
+				return Api.history_usr().$promise.then(function(data) {
+					angular.forEach(data, function(value, key) {
+						data[key]["duracion"] = data[key]["duracion"]+' hs'
+						data[key]["horario"] = data[key]["horario"]+' hs'
+						data[key]["fecha_fixed"] = value.dia+' '+dateFormat(value.fecha) ;
+					});
+					data,$scope.alumno = $scope.condicionesClases(data,$scope.alumno);
+					$scope.clases = data;
+					// Filter & Sort
+					orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
+					// Show
+					params.total(orderedData.inlineCount);
+					stopLoading();
+					return orderedData;
+				});
+			}
+		});
+	});
+	// eventModal
+	$scope.eventModal = function(clase_id) {
+		$scope.clase = $.grep($scope.clases, function(e){ return e.id == clase_id; })[0];
+		$('#events-modal').modal('show');
+	};
+	// Unjoin
+	$scope.UnJoinUser = function() {
+		startLoading();
+		$rootScope.got_to_url_success = "/app/mis_clases";
+		$cacheFactory.get('$http').remove("/api/clases/history_usr");
+		ResourceClase.unjoin($scope.deleteVariablesClaseToSend($scope.clase,true,true), $scope.callbackSuccess, $scope.callbackFailure).$promise.then(function(data) {
+			$('#alert-container').hide().html('<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-times" aria-hidden="true"></i> Clase cancelada! </strong> Ya hemos cancelado tu inscripción a la clase ¡Gracias por avisar!</div>').slideDown();
+			$scope.tableParams.reload();
+		});
+	};
+}]);
