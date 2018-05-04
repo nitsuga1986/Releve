97958d3abc2beed12a499ef90f0cf28cd7f496e2
Agustin Manceñido <agustinmancenido@gmail.com>
2018-05-04 17:29:59 -0300

    stop mailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,56 +3,56 @@ class UserMailer < ActionMailer::Base
 
 	def welcome_email(user)
 		@user = user
-		mail(to: @user.email, subject: 'Te damos la bienvenida a Releve Pilates!')
+		#mail(to: @user.email, subject: 'Te damos la bienvenida a Releve Pilates!')
 	end
 
 	def join_email(user,clase)
 		@user = user
 		@clase = clase
-		mail(to: @user.email, subject: 'Agendado: '+clase.actividad.nombre+' el '+I18n.t('date.day_names')[@clase.fecha.wday]+' '+@clase.fecha.strftime("%d/%m/%Y")+' a las '+clase.horario+' con '+clase.instructor.nombre_completo)
+		#mail(to: @user.email, subject: 'Agendado: '+clase.actividad.nombre+' el '+I18n.t('date.day_names')[@clase.fecha.wday]+' '+@clase.fecha.strftime("%d/%m/%Y")+' a las '+clase.horario+' con '+clase.instructor.nombre_completo)
 	end
 
 	def join_multiple_email(user,clases)
 		@user = user
 		@clases = clases
-		mail(to: @user.email, subject: 'Hemos agendado tus clases')
+		#mail(to: @user.email, subject: 'Hemos agendado tus clases')
 	end
 
 	def unjoin_multiple_email(user,clases)
 		@user = user
 		@clases = clases
-		mail(to: @user.email, subject: 'Hemos cancelado tus clases')
+		#mail(to: @user.email, subject: 'Hemos cancelado tus clases')
 	end
 
 	def unjoin_email(user,clase)
 		@user = user
 		@clase = clase
-		mail(to: @user.email, subject: 'Inscripción cancelada: '+clase.actividad.nombre+' el '+I18n.t('date.day_names')[@clase.fecha.wday]+' '+@clase.fecha.strftime("%d/%m/%Y")+' a las '+clase.horario+' con '+clase.instructor.nombre_completo)
+		#mail(to: @user.email, subject: 'Inscripción cancelada: '+clase.actividad.nombre+' el '+I18n.t('date.day_names')[@clase.fecha.wday]+' '+@clase.fecha.strftime("%d/%m/%Y")+' a las '+clase.horario+' con '+clase.instructor.nombre_completo)
 	end
 
 	def unjoin_comment_email(user,clase,comentario)
 		@user = user
 		@clase = clase
 		@comentario = comentario
-		mail(to: 'relevepilates@gmail.com', subject: user.nombre_completo+' dejó un comentario al cancelar la clase del '+I18n.t('date.day_names')[@clase.fecha.wday]+' '+@clase.fecha.strftime("%d/%m/%Y")+' a las '+clase.horario+' con '+clase.instructor.nombre_completo)
+		#mail(to: 'relevepilates@gmail.com', subject: user.nombre_completo+' dejó un comentario al cancelar la clase del '+I18n.t('date.day_names')[@clase.fecha.wday]+' '+@clase.fecha.strftime("%d/%m/%Y")+' a las '+clase.horario+' con '+clase.instructor.nombre_completo)
 	end
-	
+
 	def waitlist_email(clase)
 		@clase = clase
 		emails = clase.wait_users.collect(&:email).join(",")
-		mail(to: 'relevepilates@gmail.com', bcc: emails,  subject: 'Tenemos un lugar para vos!')
+		#mail(to: 'relevepilates@gmail.com', bcc: emails,  subject: 'Tenemos un lugar para vos!')
 	end
 
 	def pricing_email(email,nombre,apellido)
 		@nombre = nombre
 		@apellido = apellido
-		mail(to: email, subject: 'Info Releve')
+		#mail(to: email, subject: 'Info Releve')
 	end
 
 	def remainder_email(user,clases)
 		@user = user
 		@clases = clases
-		mail(to: @user.email, subject: 'Tu semana Releve!')
+		#mail(to: @user.email, subject: 'Tu semana Releve!')
 	end
 
 	def newsletter_email(recipients, mail_subject, mail_title, mail_pretext, mail_body, mail_button_text, mail_button_link, mail_subtitle, mail_subbody, include_reminder)
@@ -64,8 +64,8 @@ class UserMailer < ActionMailer::Base
 		@mail_subtitle = mail_subtitle
 		@mail_subbody = mail_subbody
 		@include_reminder = include_reminder
-		mail(:to => "relevepilates@gmail.com", :subject => mail_subject, :bcc => recipients)
+		#mail(:to => "relevepilates@gmail.com", :subject => mail_subject, :bcc => recipients)
 	end
 
-	
+
 end
